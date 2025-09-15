5bd889cb2b54d4cb852178811bc004b3453d0c82
SylvaticGhost <k.tkachov05@gmail.com>
2025-09-16 00:58:21 +0300

    set up prettier and editorconfig

--- script/init-db.ts
+++ script/init-db.ts
@@ -1,11 +1,11 @@
-import { Database } from "@db/sqlite";
-import { ensureDir } from "@std/fs";
+import { Database } from '@db/sqlite';
+import { ensureDir } from '@std/fs';
 
-await ensureDir("volume/schemas");
+await ensureDir('volume/schemas');
 
-const dbPath = "volume/apilens.db";
+const dbPath = 'volume/apilens.db';
 const db = new Database(dbPath);
 
 db.close();
 
-console.log("Database initialized at", dbPath);
+console.log('Database initialized at', dbPath);
